e793766aa573a0ee65b11640b5c02b5d1f124269
Obed John <73648003+osjohn01@users.noreply.github.com>
2024-04-12 19:33:21 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,7 @@ app_name = 'djangoapp'
 urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='register', view=views.registration, name='register'),
-
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for login
     path(route='login', view=views.login_user, name='login'),
     # path(route='login', view=views.login_user, name='login'),
