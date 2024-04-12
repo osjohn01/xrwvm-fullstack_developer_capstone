63b10b9a67e5f32a408d1c18032bdc1c34a4599e
Obed John <73648003+osjohn01@users.noreply.github.com>
2024-04-12 22:25:20 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,25 +1,19 @@
-# Uncomment the required imports before adding the code
-
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.contrib import messages
 from datetime import datetime
 from .models import CarMake, CarModel
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .restapis import get_request, analyze_review_sentiments, post_review
 
-
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
 
 # Create your views here.
 
@@ -76,6 +70,7 @@ def registration(request):
     else :
         data = {"userName": username,"error": "Already Registered"}
         return JsonResponse(data)
+
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
@@ -87,7 +82,7 @@ def get_cars(request):
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
 
-#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+# Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
@@ -95,7 +90,6 @@ def get_dealerships(request, state="All"):
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
-
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
@@ -110,10 +104,7 @@ def get_dealer_reviews(request, dealer_id):
     else:
         return JsonResponse({"status": 400,"message": "Bad Request"})
 
-
 # Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
 def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
@@ -122,10 +113,7 @@ def get_dealer_details(request, dealer_id):
     else:
         return JsonResponse({"status": 400,"message": "Bad Request"})
 
-
 # Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
